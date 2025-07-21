b446294c1138968d048c13bd0672d59e83a132ab
Wiktor Latanowicz <wiktor@latanowicz.com>
2025-07-21 21:04:04 +0200

    Retry wmts init

--- src/layers/wmts.ts
+++ src/layers/wmts.ts
@@ -2,16 +2,28 @@ import TileLayer from "ol/layer/Tile";
 import WMTS, { optionsFromCapabilities } from "ol/source/WMTS.js";
 import WMTSCapabilities from "ol/format/WMTSCapabilities.js";
 import type { CommonLayerJson } from ".";
+import fetchRetry from "fetch-retry";
+
+const fetchWithRetry = fetchRetry(fetch);
 
 export type WmtsLayerJson = CommonLayerJson & {
   url: string;
   layer: string;
+  init_reties?: number;
+  init_retry_delay?: number;
 };
+
+const DEFAULT_INIT_RETRIES = 5;
+const DEFAULT_INIT_RETRY_DELAY = 2000;
 
 export const wmtsLayerFromJson = async (json: WmtsLayerJson) => {
   const capabilitiesUrlParams = "?Service=WMTS&Request=GetCapabilities";
-  const capabilitiesResponse = await fetch(
-    `${json.url}${capabilitiesUrlParams}`
+  const capabilitiesResponse = await fetchWithRetry(
+    `${json.url}${capabilitiesUrlParams}`,
+    {
+      retries: json.init_reties ?? DEFAULT_INIT_RETRIES,
+      retryDelay: json.init_retry_delay ?? DEFAULT_INIT_RETRY_DELAY,
+    }
   );
   const capabilitiesText = await capabilitiesResponse.text();
   const parser = new WMTSCapabilities();
