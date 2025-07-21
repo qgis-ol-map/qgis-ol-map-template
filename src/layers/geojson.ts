c3f066dc479f020e6bf857e2ac0f5415fe02a751
Wiktor Latanowicz <wiktor@latanowicz.com>
2025-07-21 21:04:04 +0200

    Vector feature styles

--- src/layers/geojson.ts
+++ src/layers/geojson.ts
@@ -2,9 +2,11 @@ import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import GeoJSON from "ol/format/GeoJSON.js";
 import type { CommonLayerJson } from ".";
+import { makeStyleFunction, type VectorFeatureStyleJson } from "./styles/vector-feature";
 
 export type GeoJsonLayerJson = CommonLayerJson & {
   url: string;
+  style?: VectorFeatureStyleJson;
 };
 
 export const geoJsonLayerFromJson = async (json: GeoJsonLayerJson) => {
@@ -15,11 +17,14 @@ export const geoJsonLayerFromJson = async (json: GeoJsonLayerJson) => {
     ...json.sourceParams,
   });
 
+  const styleFunction = makeStyleFunction(json.style);
+
   return new VectorLayer({
     opacity: json.opacity ?? 1.0,
     zIndex: json.zIndex ?? null,
     visible: json.visible ?? true,
     source,
+    style: styleFunction,
     ...json.layerParams,
   });
 };
---
+++ src/layers/styles/vector-feature.ts
@@ -0,0 +1,83 @@
+import Style from "ol/style/Style";
+import CircleStyle from "ol/style/Circle";
+import Fill from "ol/style/Fill";
+import Stroke from "ol/style/Stroke";
+import Text from "ol/style/Text";
+import type { FeatureLike } from "ol/Feature";
+import memoize from "memoize";
+
+export type VectorFeatureStyleJson = {
+  symbol_size?: number;
+  symbol_size_unit?: string;
+  symbol_fill_color?: string;
+  symbol_stroke_color?: string;
+  symbol_stroke_width?: number;
+  symbol_stroke_width_unit?: string;
+  label_text_field?: string;
+  label_text_color?: string;
+  label_font_family?: string;
+  label_font_size?: number;
+  label_font_size_unit?: string;
+  label_font_weight?: number;
+  label_font_italic?: boolean;
+  label_outline_enabled?: boolean;
+  label_outline_color?: string;
+  label_outline_width?: number;
+  label_outline_width_unit?: string;
+};
+
+const formatFont = (json: VectorFeatureStyleJson) => {
+  const weight = json.label_font_weight ?? "normal";
+  const size = json.label_font_size ?? 10;
+  const unit = json.label_font_size_unit ?? "px";
+  const family = json.label_font_family ?? "sans-serif";
+  return `${weight} ${size}${unit} ${family}`;
+};
+
+const sizeAsPixels = (size?: number, unit?: string) => {
+  if (size == undefined) return undefined;
+  let multiplier = 1.0;
+  if (unit == "mm") multiplier = 3.75;
+  if (unit == "pt") multiplier = 1.333;
+  return size * multiplier * window.devicePixelRatio;
+};
+
+export const makeStyleFunction = (json?: VectorFeatureStyleJson) => {
+  if (!json) return undefined;
+
+  const size = sizeAsPixels(json.symbol_size ?? 5) ?? 5;
+
+  const styleFunction = memoize((feature: FeatureLike, resolution: number) => {
+    return new Style({
+      image: new CircleStyle({
+        radius: size,
+        fill: new Fill({ color: json.symbol_fill_color }),
+        stroke: new Stroke({
+          color: json.symbol_stroke_color,
+          width: sizeAsPixels(
+            json.symbol_stroke_width,
+            json.symbol_stroke_width_unit
+          ),
+        }),
+      }),
+      text: json.label_text_field
+        ? new Text({
+            text: feature.get(json.label_text_field),
+            font: formatFont(json),
+            offsetY: -2 * size,
+            fill: new Fill({ color: json.label_text_color }),
+            stroke: json.label_outline_enabled
+              ? new Stroke({
+                  color: json.label_outline_color,
+                  width: sizeAsPixels(
+                    json.label_outline_width,
+                    json.label_outline_width_unit
+                  ),
+                })
+              : undefined,
+          })
+        : undefined,
+    });
+  });
+  return styleFunction;
+};
