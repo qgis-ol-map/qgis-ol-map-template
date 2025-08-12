ee30e613249de04a0602708c3f6cfb61e120137e
Wiktor Latanowicz <wiktor@latanowicz.com>
2025-08-12 11:44:01 +0200

    Add support for geotiffs

---
+++ src/layers/geotiff.ts
@@ -0,0 +1,27 @@
+import type { CommonLayerJson } from ".";
+import GeoTIFF from 'ol/source/GeoTIFF.js';
+import TileLayer from 'ol/layer/WebGLTile.js';
+
+export type GeoTiffLayerJson = CommonLayerJson & {
+    url: string;
+    crs?: string;
+  };
+
+export const geoTiffLayerFromJson = async (json: GeoTiffLayerJson) => {
+  const source = new GeoTIFF({
+    sources: [
+      {
+        url: json.url,
+        ...json.sourceParams,
+      }
+    ],
+  });
+
+  return new TileLayer({
+    opacity: json.opacity ?? 1.0,
+    zIndex: json.zIndex ?? null,
+    visible: json.visible ?? true,
+    source: source,
+    ...json.layerParams,
+  });
+};
